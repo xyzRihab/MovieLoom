d9f785b35beb1e5cb1b0885fcfdccd30bf7b98de
Rihab <rihab.arioui@gmail.com>
2024-05-27 14:57:14 +0100

    feat: Added Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ChakraProvider, Grid } from '@chakra-ui/react';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <ChakraProvider>
       <Grid>
         <Header />
+        <Home />
       </Grid>
     </ChakraProvider>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import MovieCard from './MovieCard';
+import { Grid } from '@chakra-ui/react';
+
+const apiKey = process.env.API_KEY;
+
+export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async (searchTerm, limit = 10) => {
+    let moviesList = [];
+
+    while (moviesList.length < limit) {
+      const response = await fetch(
+        `https://www.omdbapi.com/?s=${searchTerm}&apikey=${apiKey}`
+      );
+      const data = await response.json();
+
+      if (data.Response === 'True') {
+        moviesList = moviesList.concat(data.Search);
+        if (moviesList.length >= limit) break;
+      } else {
+        break;
+      }
+    }
+
+    const detailedMovies = [];
+    for (const movie of moviesList) {
+      const movieDetailsResponse = await fetch(
+        `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${apiKey}`
+      );
+      const movieDetails = await movieDetailsResponse.json();
+      detailedMovies.push(movieDetails);
+    }
+
+    setMovies(detailedMovies);
+  };
+
+  useEffect(() => {
+    fetchMovies('Batman');
+  }, []);
+
+  return (
+    <Grid templateColumns="repeat(5, 1fr)" gap={4} p={4}>
+      {movies.map(movie => (
+        <MovieCard
+          key={movie.imdbID}
+          imdbID={movie.imdbID}
+          Title={movie.Title}
+          Plot={movie.Plot}
+          Genre={movie.Genre}
+          Poster={movie.Poster}
+        />
+      ))}
+    </Grid>
+  );
+}
